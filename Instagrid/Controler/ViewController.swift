d118750d32b8ce771e35b795d167c0a11f329446
DAGUIN Sébastien <sebastien.daguin972@gmail.com>
2021-03-04 18:01:09 +0100

    Mise a jour, modification du code refactorisation ajout d’une classe qui implémente la logique des layout et le déplacement des images

--- Instagrid/Controler/ViewController.swift
+++ Instagrid/Controler/ViewController.swift
@@ -10,165 +10,70 @@ import UIKit
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     //MARK: - Outlet
     @IBOutlet weak private var imageField: UIView!
-    @IBOutlet weak private var pickPhotoRightTop: UIButton!
-    @IBOutlet weak private var pickPhotoRightBottom: UIButton!
-    @IBOutlet weak private var pickPhotoLeftBottom: UIButton!
-    @IBOutlet weak private var pickPhotoLeftTop: UIButton!
-    
+    @IBOutlet weak private var pickPhotoRightTopButton: UIButton!
+    @IBOutlet weak private var pickPhotoRightBottomButton: UIButton!
+    @IBOutlet weak private var pickPhotoLeftBottomButton: UIButton!
+    @IBOutlet weak private var pickPhotoLeftTopButton: UIButton!
     @IBOutlet weak private var buttonLayout1: UIButton!
     @IBOutlet weak private var buttonLayout2: UIButton!
     @IBOutlet weak private var buttonLayout3: UIButton!
     @IBOutlet weak private var swipeToShareLabel: UILabel!
-    @IBOutlet weak private var arrowUpLabel: UIImageView!
+    @IBOutlet weak private var arrowUpView: UIImageView!
     @IBOutlet weak private var instagridLabel: UILabel!
-    @IBOutlet weak private var arrowLeft: UIImageView!
+    @IBOutlet weak private var arrowLeftView: UIImageView!
     @IBOutlet weak private var swipeLeftToShareLabel: UILabel!
     
-    private var ImageView: UIButton!
-    private var imageBackground: UIView!
-    private var isShared = true
+    private var imageViewButton: UIButton!
     private var savedImageRightTop : UIImage?
     private var savedImageRightBottom : UIImage?
-    var clickUser = 0
+    private var newSwape : SwapButton?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         prepareUserInterface()
     }
     //MARK:- Button Layout
-    @IBAction private func buttonLayout1(_ sender: Any) {
-        if !buttonLayout1.isSelected {
-            selectedButton(button: buttonLayout1)
-            savedImageRightTop = swapButton(button1: pickPhotoRightTop, button2: pickPhotoRightBottom)
-            resetLayout()
-            buttonDisappearanceAnimation(button: pickPhotoRightTop)
-        }else{
-            alertLayoutSelected()
-        }
+    @IBAction private func buttonLayout1(_ sender: UIButton) {
+        tappedLayout(sender: sender, layout: .layout1)
     }
-    @IBAction private func buttonLayout2(_ sender: Any) {
-        if !buttonLayout2.isSelected {
-            selectedButton(button: buttonLayout2)
-            savedImageRightBottom = swapButton(button1: pickPhotoRightBottom, button2: pickPhotoRightTop)
-            resetLayout()
-            buttonDisappearanceAnimation(button: pickPhotoRightBottom)
-        }else{
-            alertLayoutSelected()
-        }
+    
+    @IBAction private func buttonLayout2(_ sender: UIButton) {
+        tappedLayout(sender: sender, layout: .layout2)
     }
-    @IBAction private func buttonLayout3(_ sender: Any) {
-        if !buttonLayout3.isSelected {
-            selectedButton(button: buttonLayout3)
-            resetLayout()
-            if savedImageRightTop != nil {
-                pickPhotoRightTop.setImage(savedImageRightTop, for: .normal)
-            }
-        }else{
-            alertLayoutSelected()
-        }
+    @IBAction private func buttonLayout3(_ sender: UIButton) {
+        tappedLayout(sender: sender, layout: .layout3)
     }
-    func alertLayoutSelected() {
-        if clickUser >= 3 {
-            let alertLayout = UIAlertController(title: nil, message: "This layout is already selected", preferredStyle: .alert)
-            alertLayout.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-            present(alertLayout, animated: true) {
-                self.clickUser = 0
-            }
-        }
-        clickUser += 1
-    }
-    //MARK:- Button Photo
-    @IBAction private func addPhotoRightTop(_ sender: Any) {
-        addPhoto(button: pickPhotoRightTop)
-    }
-    @IBAction private func addPhotoLeftTop(_ sender: Any) {
-        addPhoto(button: pickPhotoLeftTop)
-    }
-    @IBAction private func addPhotoLeftBottom(_ sender: Any) {
-        addPhoto(button: pickPhotoLeftBottom)
-    }
-    @IBAction private func addPhotoRightBottom(_ sender: Any) {
-        addPhoto(button: pickPhotoRightBottom)
-    }
-    //MARK:- Add Photo
-    /// This function allows you to recover the user's photo
-    private func addPhoto(button : UIButton) {
-        ImageView = button
-        let photoSourceRequestController = UIAlertController(title: "", message: "Choose a picture ", preferredStyle: .actionSheet)
-        
-        let cameraAction = UIAlertAction(title: "Camera", style: .default) {(action ) in
-            if UIImagePickerController.isSourceTypeAvailable(.camera){
-                let imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.allowsEditing = true
-                imagePicker.sourceType = .camera
-                self.present(imagePicker, animated: true, completion: nil)
-            }
-        }
-        let photoLibraryAction = UIAlertAction(title: " Photo Library", style: .default) {(action) in
-            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
-                let imagePicker = UIImagePickerController()
-                imagePicker.delegate = self
-                imagePicker.allowsEditing = true
-                imagePicker.sourceType = .photoLibrary
-                self.present(imagePicker, animated: true, completion: nil)
-            }
-        }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
-        photoSourceRequestController.addAction(cameraAction)
-        photoSourceRequestController.addAction(photoLibraryAction)
-        photoSourceRequestController.addAction(cancelAction)
-        present(photoSourceRequestController, animated: true, completion: nil)
-    }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        if let imagePick = (info[UIImagePickerController.InfoKey.editedImage] as? UIImage){
-            ImageView.setImage(imagePick, for: .normal)
-            ImageView.contentMode = .scaleAspectFill
-            ImageView.clipsToBounds = false
-            dismiss(animated: true, completion: nil)
-        }
+    //MARK:- Buttons Photo
+    @IBAction func tappedAddPhoto(_ sender: UIButton) {
+        addPhoto(button: sender)
     }
     //MARK:- Transform Image
     ///This function moves the field
     private func transformImageField(landScape : Bool){
-        let screenHeight = UIScreen.main.bounds.height
-        let screenWidth = UIScreen.main.bounds.width
-        switch landScape {
-        case false:
-            UIView.animate(withDuration: 1.5, animations: {
-                self.imageField.transform = CGAffineTransform(translationX: 0, y: -screenHeight)
-                self.swipeToShareLabel.shake()
-                self.shareImageField()
-            })
-        case true:
-            UIView.animate(withDuration: 1.5, animations: {
-                self.imageField.transform = CGAffineTransform(translationX: -screenWidth, y: 0)
-                self.swipeToShareLabel.shake()
-                self.swipeLeftToShareLabel.shake()
-                self.shareImageField()
-            })
-        }
+        let transform = landScape ? CGAffineTransform(translationX: -UIScreen.main.bounds.width, y: 0) : CGAffineTransform(translationX: 0, y: -UIScreen.main.bounds.height)
+        let labelToShake : UILabel = landScape ? swipeLeftToShareLabel : swipeToShareLabel
+        UIView.animate(withDuration: 1.5, animations: {
+            self.imageField.transform = transform
+            self.swipeToShareLabel.shake()
+            labelToShake.shake()
+            self.shareImageField()
+        })
     }
     //MARK:- Swipe To Share Image
     ///This function allows you to select the movement according to the orientation
-    @objc  private func swipe(sender : UISwipeGestureRecognizer) {
-        let orientation = UIDevice.current.orientation
+    @objc private func swipe(sender : UISwipeGestureRecognizer) {
         switch sender.direction {
         case .left:
-            print(" Ici la direction landscape est \(orientation.isLandscape)")
-            if orientation.isLandscape {
+            if UIDevice.current.orientation.isLandscape {
                 transformImageField(landScape: true)
             }
         default:
-            let screenHeight = UIScreen.main.bounds.height
-            let screenWidth = UIScreen.main.bounds.width
-            
-            if screenHeight > screenWidth {
+            let positionPortrait : Bool = UIScreen.main.bounds.height > UIScreen.main.bounds.width
+            if positionPortrait {
                 transformImageField(landScape: false)
             }
         }
-        
     }
     //MARK:- Share Image
     ///This function transforms the field into an image and displays the share sheet
@@ -181,38 +86,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         vc.popoverPresentationController?.sourceView = self.view
         vc.excludedActivityTypes = [.assignToContact]
         vc.completionWithItemsHandler = {(activityType: UIActivity.ActivityType?, completed: Bool, returnedItems: [Any]?, error: Error?) in
-            if completed {
-                let alertVc = UIAlertController(title: "Sharing is a success", message: "Your action has been completed", preferredStyle: .alert)
-                alertVc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { _ in
-                    self.resetAllButton()
-                    UIView.animate(withDuration: 3) {
-                        self.imageField.transform = .identity
-                    }
-                }))
-                self.present(alertVc, animated: true, completion: nil)
-                return
-            }else{
-                let alertVc = UIAlertController(title: "Sharing was not successful", message: "Your action failed or was canceled", preferredStyle: .alert)
-                alertVc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { _ in
-                    UIView.animate(withDuration: 3) {
-                        self.imageField.transform = .identity
-                    }
-                }))
-                self.present(alertVc, animated: true, completion: nil)
-            }
+            let alertUser = completed ? self.alertUser(title: "Sharing is a success", message: "Your action has been completed") : self.alertUser(title: "Sharing was not successful", message: "Your action failed or was canceled")
+            self.present(alertUser, animated: true, completion: nil)
         }
         present(vc, animated: true, completion: nil)
     }
-    //MARK:- Reset Layout
-    ///This function returns all buttons to their initial display.
-    private func resetLayout() {
-        UIView.animate(withDuration: 0.5) {
-            self.pickPhotoRightTop.isHidden = false
-            self.pickPhotoRightBottom.isHidden = false
-            self.pickPhotoLeftBottom.isHidden = false
-            self.pickPhotoLeftTop.isHidden = false
-        }
+    private func alertUser(title : String ,message : String)-> UIAlertController  {
+        let alertVc = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVc.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: { _ in
+            UIView.animate(withDuration: 3) {
+                self.imageField.transform = .identity
+            }
+        }))
+        return alertVc
     }
+    //MARK:- Function and user interface preparation
     //MARK:- Prepare label
     ///This function allows you to retrieve the fonts and assignments to labels
     private func prepareLabel() {
@@ -237,12 +125,10 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         swipeLeftToShareLabel.textColor = .white
         swipeLeftToShareLabel.font = UIFontMetrics.default.scaledFont(for: customFontDelmMedium)
     }
-    ///This function is used to display the start screen
     //MARK:- Prepare user interface
+    ///This function is used to display the start screen
     private func prepareUserInterface() {
-        resetLayout()
-        pickPhotoLeftBottom.isHidden = true
-        selectedButton(button: buttonLayout2)
+        tappedLayout(sender: buttonLayout2, layout: .layout2)
         let swipeUp = UISwipeGestureRecognizer()
         swipeUp.direction = .up
         let swipeLeft = UISwipeGestureRecognizer()
@@ -253,37 +139,65 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         swipeLeft.addTarget(self, action: #selector(swipe))
         prepareLabel()
     }
-    private func swapButton(button1 : UIButton, button2 : UIButton) -> UIImage {
-        button2.setImage(button1.currentImage, for: .normal)
-        button1.setImage(#imageLiteral(resourceName: "Image"), for: .normal)
-        return button2.currentImage!
+    //MARK:- Reset all button
+    ///This function returns the images of the buttons to their defaults.
+    private func resetAllButton() {
+        pickPhotoRightTopButton.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
+        pickPhotoRightBottomButton.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
+        pickPhotoLeftBottomButton.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
+        pickPhotoLeftTopButton.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
+    }
+    //MARK:- Add Photo
+    /// This function allows you to recover the user's photo
+    private func addPhoto(button : UIButton) {
+        imageViewButton = button
+        let photoSourceRequestController = UIAlertController(title: "", message: "Choose a picture ", preferredStyle: .actionSheet)
+        let cameraAlertAction = choiceSourceType(messageAlert: "Camera", sourceType: .camera)
+        let photoLibraryAlertAction = choiceSourceType(messageAlert: "Photo Library", sourceType: .photoLibrary)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        photoSourceRequestController.addAction(cameraAlertAction)
+        photoSourceRequestController.addAction(photoLibraryAlertAction)
+        photoSourceRequestController.addAction(cancelAction)
+        present(photoSourceRequestController, animated: true, completion: nil)
+    }
+    private func choiceSourceType(messageAlert : String, sourceType : UIImagePickerController.SourceType ) -> UIAlertAction{
+        let alertAction = UIAlertAction(title: messageAlert , style: .default) {(action ) in
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = false
+            imagePicker.sourceType = sourceType
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        return alertAction
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let imagePick = (info[UIImagePickerController.InfoKey.originalImage] as? UIImage){
+            imageViewButton.setImage(imagePick, for: .normal)
+            imageViewButton.contentMode = .scaleAspectFill
+            imageViewButton.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+            imageViewButton.clipsToBounds = false
+            dismiss(animated: true, completion: nil)
+        }
     }
     private func resetButtonLayout() {
         buttonLayout1.isSelected = false
         buttonLayout2.isSelected = false
         buttonLayout3.isSelected = false
     }
-    //MARK:- Selected Button
-    private func selectedButton(button : UIButton) {
-        resetButtonLayout()
-        button.isSelected = true
-        button.contentVerticalAlignment = .fill
-        button.contentHorizontalAlignment = .fill
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    private func prepareSwap(sender : UIButton, layout : Layout) -> SwapButton {
+        let newSwap = SwapButton(sender: sender, buttonLeftTop: pickPhotoLeftTopButton, buttonLeftBottom: pickPhotoLeftBottomButton, buttonRightTop: pickPhotoRightTopButton, buttonRightBottom: pickPhotoRightBottomButton, layout: layout)
+        return newSwap
     }
-    //MARK:- Button disaspperance
-    ///Displays the animation of the buttons that disappear
-    private func buttonDisappearanceAnimation(button : UIButton) {
-        UIView.animate(withDuration: 0.5) {
-            button.isHidden = true
+    private func tappedLayout(sender : UIButton, layout : Layout) {
+        guard !sender.isSelected else {
+            present( alertUser(title: "Layout selected", message: "This layout already selected"), animated: true, completion: nil)
+            return
         }
-    }
-    //MARK:- Reset all button
-    ///This function returns the images of the buttons to their defaults.
-    private func resetAllButton() {
-        pickPhotoRightTop.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
-        pickPhotoRightBottom.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
-        pickPhotoLeftBottom.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
-        pickPhotoLeftTop.setImage(#imageLiteral(resourceName: "Plus"), for: .normal)
+        resetButtonLayout()
+        guard newSwape != nil else {
+            newSwape = prepareSwap(sender: sender, layout: layout)
+            return
+        }
+        newSwape?.tappedOnButtonLayout1and2(sender: sender, layout: layout)
     }
 }
---
+++ Instagrid/Model/SwapButton.swift
@@ -0,0 +1,108 @@
+//
+//  SwapButton.swift
+//  Instagrid
+//
+//  Created by DAGUIN Sébastien on 03/03/2021.
+//
+
+import Foundation
+import UIKit
+
+enum Layout {
+    case layout1
+    case layout2
+    case layout3
+}
+class SwapButton {
+    
+   private var sender : UIButton
+   private let buttonLeftTop : UIButton
+   private let buttonLeftBottom : UIButton
+   private let buttonRightTop: UIButton
+   private let buttonRightBottom : UIButton
+    private var layout : Layout{
+        didSet{
+            print("Old value est \(oldValue) et la nouvelle  value est \(layout)")
+            swapButton(oldLayout: oldValue, newLayout: layout)
+            if layout == .layout1 {
+                buttonDisappearanceAnimation(button: buttonRightTop)
+            }else if layout == .layout2 {
+                buttonDisappearanceAnimation(button: buttonRightBottom)
+            }
+        }
+    }
+    init(sender : UIButton, buttonLeftTop : UIButton, buttonLeftBottom : UIButton, buttonRightTop : UIButton,  buttonRightBottom : UIButton, layout : Layout ) {
+        self.sender = sender
+        self.buttonLeftTop = buttonLeftTop
+        self.buttonLeftBottom = buttonLeftBottom
+        self.buttonRightTop = buttonRightTop
+        self.buttonRightBottom = buttonRightBottom
+        self.layout = layout
+    }
+    func tappedOnButtonLayout1and2(sender : UIButton,layout : Layout) {
+        resetLayout()
+        selectedButton(button: sender)
+        setLayout(layout)
+    }
+    
+    private func setLayout(_ layout : Layout)  {
+        self.layout = layout
+    }
+    //MARK:- Selected Button
+    private func selectedButton(button : UIButton) {
+        button.isSelected = true
+        button.contentVerticalAlignment = .fill
+        button.contentHorizontalAlignment = .fill
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }
+    private func swapButton(oldLayout: Layout, newLayout: Layout){
+        let layouts = [oldLayout, newLayout]
+        let layout2to1 : Bool = layouts[0] == .layout2 && layouts[1] == .layout1
+        let layout1to2 : Bool = layouts[0] == .layout1 && layouts[1] == .layout2
+        let layout3 : Bool = layouts[1] == .layout3
+        print(layouts[0])
+        print(layouts[1])
+        
+        print("2 a 1 est \(layout2to1)")
+        print(" 1 a 2 est \(layout1to2)")
+        
+        let saveRightTop = buttonRightTop.currentImage
+        let saveRightBottom = buttonRightBottom.currentImage
+        let saveLeftTop = buttonLeftTop.currentImage
+        let saveLeftBottom = buttonLeftBottom.currentImage
+        
+        if layout2to1 {
+            let imageSwap = saveLeftBottom
+            buttonRightBottom.setImage(saveRightTop, for: .normal)
+            buttonLeftTop.setImage(imageSwap, for: .normal)
+            buttonLeftBottom.setImage(saveLeftTop, for: .normal)
+        }else if layout1to2{
+            let imageSwap = saveLeftTop
+            buttonLeftTop.setImage(saveLeftBottom, for: .normal)
+            buttonLeftBottom.setImage(imageSwap, for: .normal)
+            buttonRightTop.setImage(saveRightBottom, for: .normal)
+        }else if layout3{
+            buttonRightTop.setImage(saveRightTop, for: .normal)
+            buttonRightBottom.setImage(saveRightBottom, for: .normal)
+            buttonLeftBottom.setImage(saveLeftBottom, for: .normal)
+            buttonLeftTop.setImage(saveLeftTop, for: .normal)
+        }
+    }
+    //MARK:- Button disaspperance
+    ///Displays the animation of the buttons that disappear
+    private func buttonDisappearanceAnimation(button : UIButton) {
+        UIView.animate(withDuration: 0.5) {
+            button.isHidden = true
+        }
+    }
+    //MARK:- Reset Layout
+    ///This function returns all buttons to their initial display.
+    private func resetLayout() {
+        UIView.animate(withDuration: 0.5) {
+            self.buttonRightTop.isHidden = false
+            self.buttonRightBottom.isHidden = false
+            self.buttonLeftTop.isHidden = false
+            self.buttonLeftBottom.isHidden = false
+        }
+    }
+}
